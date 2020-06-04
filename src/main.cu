0e9e238d490e7a5cb4c281f932de77f5819665c4
brekekekex <ynakamot@u.rochester.edu>
2020-06-04 07:12:36 -0400

    more main

--- src/main.cu
+++ src/main.cu
@@ -5,15 +5,15 @@
 #include <cuda.h>
 #include <cuda_runtime.h>
 
-
-
-
-
-
+// 4K resolution (16:9)
+#define RENDER_X 3840
+#define RENDER_Y 2160
 
 int main(void)
 {
-	printf("hello world\n");
+	char *hist = (char *)malloc(sizeof(char) * SCREEN_X * SCREEN_Y);
+	assert(hist != NULL); 
+	
 	return 0;
 }
 
