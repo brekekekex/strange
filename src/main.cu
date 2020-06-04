f720e1f712b4f272c9b436dc484711fffbc31d45
brekekekex <ynakamot@u.rochester.edu>
2020-06-04 06:43:28 -0400

    start main

---
+++ src/main.cu
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <assert.h>
+#include <cuda.h>
+#include <cuda_runtime.h>
+
+
+
+
+
+
+
+int main(void)
+{
+	printf("hello world\n");
+	return 0;
+}
+
